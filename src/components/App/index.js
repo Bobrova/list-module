f72e8872f81c0a5c7b1eae770f8714a59a4bc3ac
Nastya Bobrova <deepkilt89@gmail.com>
2019-09-06 20:26:37 +0300

    first commit

---
+++ src/components/App/index.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import styles from "./style.css";
+class App extends Component {
+  render() {
+    return <div className={styles.intro}>Hello World</div>;
+  }
+}
+export default App;
